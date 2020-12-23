925d81260044b8e13747866138cca8f7068340bd
SatoshiTomita <11.1010.100.x2@gmail.com>
2020-12-23 13:41:32 +0900

    feat(config): apply config store

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,23 @@
 import chalk = require('chalk');
 import prompts = require('prompts');
 import {api} from './api';
+import Configstore = require('configstore');
+
+const appName = 'youtube-cli';
 
 export const main = (async () => {
   console.log(chalk.blue('Welcome to YouTube CLI\n'));
-  const apiKey = await prompts({
-    name: 'apiKey',
-    type: 'text',
-    message: 'API Key',
-  });
+  const configStore = new Configstore(appName);
+  if (configStore.get('apiKey') === undefined) {
+    const userInput = await prompts({
+      name: 'apiKey',
+      type: 'text',
+      message: 'API Key',
+    });
+    configStore.set('apiKey', userInput.apiKey);
+  }
+  const apiKey = configStore.get('apiKey');
+
   const userInput = await prompts({
     name: 'query',
     type: 'text',
