ee8bd94840bf57a820030bbc0104fea1ea6ec3e1
SatoshiTomita <11.1010.100.x2@gmail.com>
2020-12-24 02:48:25 +0900

    fix(mpv): stdout

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,11 @@ import chalk = require('chalk');
 import prompts = require('prompts');
 import {api} from './api';
 import Configstore = require('configstore');
-const mpv = require('node-mpv');
+import {spawn} from 'child_process';
 
 const appName = 'youtube-cli';
 
-export const main = (async () => {
-  // title
-  console.log(chalk.blue('Welcome to YouTube CLI\n'));
-
-  // apikey
+const getApiKey = async () => {
   const configStore = new Configstore(appName);
   if (!configStore.has('apiKey')) {
     const userInput = await prompts({
@@ -20,43 +16,51 @@ export const main = (async () => {
     });
     configStore.set('apiKey', userInput.apiKey);
   }
-  const apiKey = configStore.get('apiKey');
-
-  // search
+  return configStore.get('apiKey');
+};
+const searchVideos = async (apiKey: string) => {
   const userInput = await prompts({
     name: 'query',
     type: 'text',
     message: 'Search',
   });
   console.log(`Searching for ${userInput.query}...\n`);
-  const videos = await api.search.byQuery({
+  return api.search.byQuery({
     apiKey,
     query: userInput.query,
   });
+};
 
-  // choose video
+const chooseVideo = (
+  videos: {id?: string; title?: string; description?: string}[]
+) => {
   const choices = videos.map(video => ({
     title: video.title || '',
     description: video.description,
     value: video.id,
     disabled: false,
   }));
-  const userChoice = await prompts({
+  return prompts({
     type: 'select',
     name: 'videoId',
     message: 'Select a video',
     choices,
     initial: 1,
   });
+};
+
+const playYoutubeMusic = (url: string) => {
+  spawn('mpv', ['--no-video', url], {stdio: 'inherit'});
+};
+
+export const main = (async () => {
+  console.log(chalk.blue('Welcome to YouTube CLI\n'));
+  const apiKey = await getApiKey();
+  const videos = await searchVideos(apiKey);
+  const userChoice = await chooseVideo(videos);
 
   // play
-  const mpvPlayer = new mpv(
-    {
-      // verbose: true,
-      audio_only: true,
-    }
-    // ['--fullscreen', '--fps=60']
-  );
   const url = `https://www.youtube.com/watch?v=${userChoice.videoId}`;
-  mpvPlayer.load(url);
+  console.log('Play: ', url);
+  playYoutubeMusic(url);
 })();
