98242ef0c32320f4bdda01368bea0d0e734e9432
SatoshiTomita <11.1010.100.x2@gmail.com>
2020-12-24 03:21:12 +0900

    fix: some error

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import prompts = require('prompts');
 import {api} from './api';
 import Configstore = require('configstore');
 import {spawn} from 'child_process';
+// import {textSync} from 'figlet';
 
 const appName = 'youtube-cli';
 
@@ -23,7 +24,9 @@ const searchVideos = async (apiKey: string) => {
     name: 'query',
     type: 'text',
     message: 'Search',
+    validate: text => (text.length > 0 ? true : false),
   });
+  if (userInput.query === undefined) throw new Error('undefined');
   console.log(`Searching for ${userInput.query}...\n`);
   return api.search.byQuery({
     apiKey,
@@ -36,7 +39,7 @@ const chooseVideo = (
 ) => {
   const choices = videos.map(video => ({
     title: video.title || '',
-    description: video.description,
+    // description: video.description,
     value: video.id,
     disabled: false,
   }));
@@ -45,22 +48,28 @@ const chooseVideo = (
     name: 'videoId',
     message: 'Select a video',
     choices,
-    initial: 1,
+    initial: 0,
   });
 };
 
-const playYoutubeMusic = (url: string) => {
+const playYoutubeMusic = (videoId: string) => {
+  if (videoId === undefined) throw new Error('undefined');
+  const url = `https://www.youtube.com/watch?v=${videoId}`;
+  console.log('Play: ', url);
   spawn('mpv', ['--no-video', url], {stdio: 'inherit'});
 };
 
 export const main = (async () => {
-  console.log(chalk.blue('Welcome to YouTube CLI\n'));
+  console.log(chalk.red('Welcome to YouTube CLI\n'));
+  // console.log(
+  //   chalk.red(
+  //     textSync('YouTube CLI', {
+  //       font: 'Standard',
+  //     })
+  //   )
+  // );
   const apiKey = await getApiKey();
   const videos = await searchVideos(apiKey);
   const userChoice = await chooseVideo(videos);
-
-  // play
-  const url = `https://www.youtube.com/watch?v=${userChoice.videoId}`;
-  console.log('Play: ', url);
-  playYoutubeMusic(url);
+  playYoutubeMusic(userChoice.videoId);
 })();
