73d1f1560bf103018a5c4beba9d4ff7bdccad938
SatoshiTomita <11.1010.100.x2@gmail.com>
2020-12-23 15:16:36 +0900

    feat(play): open youtube with mpv

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,15 @@ import chalk = require('chalk');
 import prompts = require('prompts');
 import {api} from './api';
 import Configstore = require('configstore');
+const mpv = require('node-mpv');
 
 const appName = 'youtube-cli';
 
 export const main = (async () => {
+  // title
   console.log(chalk.blue('Welcome to YouTube CLI\n'));
+
+  // apikey
   const configStore = new Configstore(appName);
   if (!configStore.has('apiKey')) {
     const userInput = await prompts({
@@ -18,15 +22,41 @@ export const main = (async () => {
   }
   const apiKey = configStore.get('apiKey');
 
+  // search
   const userInput = await prompts({
     name: 'query',
     type: 'text',
     message: 'Search',
   });
   console.log(`Searching for ${userInput.query}...\n`);
-  const result = await api.search.byQuery({
+  const videos = await api.search.byQuery({
     apiKey: apiKey.apiKey,
     query: userInput.query,
   });
-  console.log('result: ', result);
+
+  // choose video
+  const choices = videos.map(video => ({
+    title: video.title || '',
+    description: video.description,
+    value: video.id,
+    disabled: false,
+  }));
+  const userChoice = await prompts({
+    type: 'select',
+    name: 'videoId',
+    message: 'Select a video',
+    choices,
+    initial: 1,
+  });
+
+  // play
+  const mpvPlayer = new mpv(
+    {
+      // verbose: true,
+      audio_only: true,
+    }
+    // ['--fullscreen', '--fps=60']
+  );
+  const url = `https://www.youtube.com/watch?v=${userChoice.videoId}`;
+  mpvPlayer.load(url);
 })();
